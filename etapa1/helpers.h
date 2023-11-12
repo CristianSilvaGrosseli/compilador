28aa294fef1974f48cd810680abfbf4060b763df
CristianSilvaGrosseli <cristian23sai@gmail.com>
2023-11-12 14:04:24 -0300

    Etapa 1 completa e sem erros

---
+++ etapa1/helpers.h
@@ -0,0 +1,12 @@
+// Entrega 1
+// Cristian Silva Grosseli - 00243693
+// Iuri Mendonça Tinti - 00278043
+
+extern int yylineno;
+
+int get_line_number();
+
+int get_line_number()
+{
+    return yylineno;
+}
